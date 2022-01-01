ffe81ee043a92d54a53feadc46a2e73ecdb337e2
deepness1 <mojyack.shadow@gmail.com>
2022-01-01 14:31:53 +0900

    adopt to server changes

--- src/hitomi/l2b.py
+++ src/hitomi/l2b.py
@@ -1,13 +1,7 @@
-on = []
-total = 0
+off = []
 for line in open("/tmp/list"):
-    o1 = line.find('=') + 2
-    o2 = line.find(';', o1)
-    if line[o1:o2] == '1':
-        p1 = line.find(' ') + 1
-        p2 = line.find(' ', p1) - 1
-        on.append(int(line[p1:p2]))
-    total += 1
+    n = int(line[4:-2])
+    off.append(n)
 
-for i in range(total):
-    print('1, ' if i in on else '0, ', end = None if (i + 1) % 32 == 0 else '')
+for i in range(4096):
+    print('0, ' if i in off else '1, ', end = None if (i + 1) % 32 == 0 else '')
