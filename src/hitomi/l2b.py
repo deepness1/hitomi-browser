ef11c9ab3ab45d8f3077dd834c3825be0600d10a
deepness1 <mojyack.shadow@gmail.com>
2022-01-16 23:57:03 +0900

    adopt to server changes

--- src/hitomi/l2b.py
+++ src/hitomi/l2b.py
@@ -1,7 +1,14 @@
+def parse_line(line):
+    p = line.find(')', 10)
+    g = line[10:p]
+    o = line[p + 8]
+    return (g, o)
+
 on = []
 for line in open("/tmp/list"):
-    n = int(line[4:-2])
-    on.append(n)
+    r = parse_line(line)
+    if r[1] == '1':
+        on.append(int(r[0]))
 
 for i in range(4096):
     print('1, ' if i in on else '0, ', end = None if (i + 1) % 32 == 0 else '')
