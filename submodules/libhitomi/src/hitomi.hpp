02c458919b50f48b47563ed4de174b0bccd5a52e
mojyack <mojyack@gmail.com>
2025-07-10 19:30:38 +0900

    migrate to submod

--- submodules/libhitomi/src/hitomi.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace hitomi {
-auto init_hitomi() -> bool;
-auto finish_hitomi() -> bool;
-} // namespace hitomi
--- submodules/libhitomi/src/type.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <cstdint>
-
-namespace hitomi {
-using GalleryID = uint32_t;
-} // namespace hitomi
